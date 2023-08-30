07ec127d14cd10f886ba75447801202ab7c142b6
Xinze-Zheng <xinzzheng.work@gmail.com>
2023-08-29 21:57:48 -0500

    python log generator

---
+++ log_generation.py
@@ -0,0 +1,26 @@
+import datetime
+import random
+
+test_pattern = {"high frequency": 1e6, "mid frequency": 1e4, "low frequency": 1e2}
+
+def inject_patter(f: __file__):
+    if len(test_pattern) > 0:
+        # 80% chance to inject a pattern
+        if random.randint(1, 10) <= 8:
+            choice = random.choice(list(test_pattern.keys()))
+            test_pattern[choice] -= 1
+            f.write(" " + str(choice))
+            if test_pattern[choice] <= 0:
+                del test_pattern[choice]
+
+
+def main():
+    with open("./log", "w") as f:
+        for i in range(2000000):
+            f.write(f"{datetime.datetime.now()} {random.randint(0, 9)}")
+            inject_patter(f)
+            f.write("\n")
+        print(test_pattern)
+
+if __name__ == "__main__":
+    main()
