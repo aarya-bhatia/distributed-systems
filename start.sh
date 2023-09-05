91feb950078e7955ac09831a78675aac5436ad93
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-09-05 16:51:30 -0500

    update start.sh script

--- start.sh
+++ start.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-if [ $# -lt 3 ]; then
-	printf "Usage: %s ID PORT\n" $0
+line=$(cat hosts | grep -m 1 -i "$(hostname)")
+
+if [ ! -z "$line" ]; then
+	id=$(echo "$line" | cut -d' ' -f1)
+	host=$(echo "$line" | cut -d' ' -f2)
+	port=$(echo "$line" | cut -d' ' -f3)
+fi
+
+if [ -z $id ] || [ -z $host ] || [ -z $port ]; then
+	echo "Failed to read server information"
 	exit 1
 fi
 
-ID=$1
-PORT=$2
+echo $id, $host, $port
 
-cd $HOME
-
-if [ ! -e mp1 ]; then
-	git clone ssh://git@gitlab.engr.illinois.edu/aaryab2/mp1.git
+if [ ! -e "$HOME/mp1" ]; then
+	git clone ssh://git@gitlab.engr.illinois.edu/aaryab2/mp1.git $HOME/mp1
 	if [ ! $? -eq 0 ]; then
-		echo "failed to clone repository"
+		echo "Failed to clone repository"
 		exit 1
 	fi
 fi
 
-cd mp1
-git pull origin main
+cd ~/mp1
+git pull
 make clean
 make
-
 pkill server
-nohup bin/server $ID $PORT 2>&1 > "$hostname.out" &
-
+nohup bin/server $id $port 2>&1 > "$hostname.out" &
 
