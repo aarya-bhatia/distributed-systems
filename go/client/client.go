ec3cb5cf92bb271ec21f49149147c6d409e30955
Xinze-Zheng <xinzzheng.work@gmail.com>
2023-09-05 13:04:04 -0500

    add metadata to client

--- go/client/client.go
+++ go/client/client.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"os"
 	"strings"
+	"time"
 )
 
 type Host struct {
@@ -15,19 +16,31 @@ type Host struct {
 	port string
 }
 
+type MetaData struct {
+	latency  string
+	dataSize int
+	lines    int
+	host     Host
+}
+
 func main() {
+	if len(os.Args) <= 1 {
+		log.Fatal("Please enter command")
+	}
 	hosts := readHosts()
 
 	outputChannel := make(chan string)
 	finishedChannel := make(chan bool)
+	metaDataChannel := make(chan MetaData, 4096)
 
 	argsWithoutProg := strings.Join(os.Args[1:], " ")
 	cmd := []byte(argsWithoutProg + "\n")
 
 	for _, host := range hosts {
-		go connect(host, outputChannel, finishedChannel, cmd)
+		go connect(host, outputChannel, finishedChannel, metaDataChannel, cmd)
 	}
 
+	// Routine that check if all communication are finished
 	go func() {
 		count := 0
 
@@ -37,11 +50,20 @@ func main() {
 		}
 
 		close(outputChannel)
+		close(metaDataChannel)
 	}()
+	totalLines := 0
 
 	for str := range outputChannel {
 		fmt.Print(str)
+		totalLines++
 	}
+	fmt.Println("---Meta data---")
+	for metaData := range metaDataChannel {
+		hostSignature := fmt.Sprintf("%s %s:%s", metaData.host.id, metaData.host.host, metaData.host.port)
+		fmt.Printf("%s, lines: %d, data: %d, latency: %s\n", hostSignature, metaData.lines, metaData.dataSize, metaData.latency)
+	}
+	fmt.Printf("Total of %d lines received from server\n", totalLines)
 }
 
 func readHosts() []Host {
@@ -72,25 +94,32 @@ func readHosts() []Host {
 	return hosts
 }
 
-func connect(host Host, channel chan string, finishedChannel chan bool, cmd []byte) {
+func connect(host Host, channel chan string, finishedChannel chan bool, metaDataChannel chan MetaData, cmd []byte) {
 	conn, err := net.Dial("tcp", host.host+":"+host.port)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("Connected to: " + conn.LocalAddr().String())
+	serverSignature := fmt.Sprintf("%s %s:%s", host.id, host.host, host.port)
+	fmt.Println("Connected to: " + serverSignature)
 	_, err = conn.Write(cmd)
 	conn.(*net.TCPConn).CloseWrite()
+	startTime := time.Now()
 	connbuf := bufio.NewReader(conn)
-
+	dataTransferred := 0
+	lineCount := 0
 	for {
 		str, err := connbuf.ReadString('\n')
 
 		if err != nil {
+			metaData := MetaData{time.Now().Sub(startTime).String(), dataTransferred, lineCount, host}
+			metaDataChannel <- metaData
 			finishedChannel <- true
 			break
 		}
-
+		dataTransferred += len(str)
 		if str != "\n" {
+			lineCount++
+			str = host.id + " " + host.host + ":" + host.port + " " + str
 			channel <- str
 		}
 	}
