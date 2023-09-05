e71b19d16971e5e507b0500080c90a2207723913
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-09-05 14:09:35 -0500

    Updated client code

--- go/client/client.go
+++ go/client/client.go
@@ -7,20 +7,76 @@ import (
 	"net"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
 type Host struct {
-	id   string
-	host string
-	port string
-}
-
-type MetaData struct {
+	id       string
+	host     string
+	port     string
 	latency  string
 	dataSize int
 	lines    int
-	host     Host
+}
+
+type Queue[T any] struct {
+	data    []T
+	m       sync.Mutex
+	cv      sync.Cond
+	waiting int
+}
+
+func NewHost(id string, host string, port string) *Host {
+	obj := new(Host)
+	obj.id = id
+	obj.host = host
+	obj.port = port
+	obj.latency = ""
+	obj.dataSize = 0
+	obj.lines = 0
+
+	return obj
+}
+
+func (q *Queue[T]) init() {
+	q.cv = *sync.NewCond(&q.m)
+}
+
+func (q *Queue[T]) push(value T) {
+	q.m.Lock()
+	q.data = append(q.data, value)
+	q.waiting = 0
+	q.cv.Signal()
+	// fmt.Println("push(): Queue: ", q.data)
+	q.m.Unlock()
+}
+
+func (q *Queue[T]) pop() T {
+	q.m.Lock()
+	q.waiting += 1
+	for len(q.data) == 0 {
+		q.cv.Wait()
+	}
+	value := q.data[0]
+	q.data = q.data[1:]
+	// fmt.Println("pop(): Queue: ", q.data)
+	q.waiting -= 1
+	if q.waiting > 0 {
+		q.cv.Signal()
+	}
+	q.m.Unlock()
+	return value
+}
+
+func (q *Queue[T]) empty() bool {
+	q.m.Lock()
+	if len(q.data) == 0 {
+		q.m.Unlock()
+		return true
+	}
+	q.m.Unlock()
+	return false
 }
 
 func main() {
@@ -28,17 +84,38 @@ func main() {
 		log.Fatal("Please enter command")
 	}
 	hosts := readHosts()
-
-	outputChannel := make(chan string)
 	finishedChannel := make(chan bool)
-	metaDataChannel := make(chan MetaData, 4096)
-
 	argsWithoutProg := strings.Join(os.Args[1:], " ")
 	cmd := []byte(argsWithoutProg + "\n")
 
+	var queue *Queue[string] = &Queue[string]{}
+	queue.init()
+
+	totalLines := 0
+
+	// Create a new thread for each connection
 	for _, host := range hosts {
-		go connect(host, outputChannel, finishedChannel, metaDataChannel, cmd)
+		go connect(host, queue, finishedChannel, cmd)
 	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// Thread to receive log messages from the workers and print to stdout
+	go func() {
+		defer wg.Done()
+		for true {
+			// fmt.Println("Queue is waiting")
+			value := queue.pop()
+			fmt.Print(value)
+
+			if value == "EXIT" {
+				return
+			}
+
+			totalLines += 1
+		}
+	}()
 
 	// Routine that check if all communication are finished
 	go func() {
@@ -49,25 +126,24 @@ func main() {
 			count += 1
 		}
 
-		close(outputChannel)
-		close(metaDataChannel)
+		queue.push("EXIT")
 	}()
-	totalLines := 0
 
-	for str := range outputChannel {
-		fmt.Print(str)
-		totalLines++
-	}
+	wg.Wait()
+
 	fmt.Println("---Meta data---")
-	for metaData := range metaDataChannel {
-		hostSignature := fmt.Sprintf("%s %s:%s", metaData.host.id, metaData.host.host, metaData.host.port)
-		fmt.Printf("%s, lines: %d, data: %d, latency: %s\n", hostSignature, metaData.lines, metaData.dataSize, metaData.latency)
+
+	for _, host := range hosts {
+		hostSignature := fmt.Sprintf("%s %s:%s", host.id, host.host, host.port)
+		fmt.Printf("%s, lines: %d, data: %d, latency: %s\n", hostSignature, host.lines, host.dataSize, host.latency)
 	}
+
 	fmt.Printf("Total of %d lines received from server\n", totalLines)
+
 }
 
-func readHosts() []Host {
-	hosts := []Host{}
+func readHosts() []*Host {
+	hosts := []*Host{}
 	file, err := os.Open("./hosts")
 	defer file.Close()
 	if err != nil {
@@ -83,7 +159,8 @@ func readHosts() []Host {
 		}
 
 		words := strings.Split(line, " ")
-		hosts = append(hosts, Host{words[0], words[1], words[2]})
+		host := NewHost(words[0], words[1], words[2])
+		hosts = append(hosts, host)
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -94,7 +171,7 @@ func readHosts() []Host {
 	return hosts
 }
 
-func connect(host Host, channel chan string, finishedChannel chan bool, metaDataChannel chan MetaData, cmd []byte) {
+func connect(host *Host, queue *Queue[string], finishedChannel chan bool, cmd []byte) {
 	conn, err := net.Dial("tcp", host.host+":"+host.port)
 	if err != nil {
 		fmt.Println(err)
@@ -111,16 +188,20 @@ func connect(host Host, channel chan string, finishedChannel chan bool, metaData
 		str, err := connbuf.ReadString('\n')
 
 		if err != nil {
-			metaData := MetaData{time.Now().Sub(startTime).String(), dataTransferred, lineCount, host}
-			metaDataChannel <- metaData
+			host.latency = time.Now().Sub(startTime).String()
+			host.dataSize = dataTransferred
+			host.lines = lineCount
+			fmt.Println(*host)
 			finishedChannel <- true
 			break
 		}
+
 		dataTransferred += len(str)
+
 		if str != "\n" {
 			lineCount++
 			str = host.id + " " + host.host + ":" + host.port + " " + str
-			channel <- str
+			queue.push(str)
 		}
 	}
 }
