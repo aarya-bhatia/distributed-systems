3eb5ea37ff30c006949bf43752b33123e416c981
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-09-07 21:06:24 -0500

    added test for vms

--- src/client/client_test.go
+++ src/client/client_test.go
@@ -2,8 +2,8 @@ package main
 
 import "testing"
 import "fmt"
+import "os/exec"
 
-const TEST_LOG_PATH = "test_logs"
 const OUTPUT_PATH = "../../outputs"
 
 func TestPattern(t *testing.T) {
@@ -14,13 +14,45 @@ func TestPattern(t *testing.T) {
 		client := RunClient(ClientArgs{
 			grep:            "grep " + pattern,
 			outputDirectory: OUTPUT_PATH,
-			logsDirectory:   TEST_LOG_PATH,
+			logsDirectory:   "test_logs",
 			silence:         true,
 			command:         "",
 		})
 
 		for _, host := range client.hosts {
 			if host.status == STATUS_SUCCESS {
+				if host.lines != count {
+					t.Fail()
+				}
+			}
+		}
+
+		fmt.Println(client.stat)
+	}
+}
+
+func TestSampleData(t *testing.T) {
+	for _, pattern := range []string{"DELETE", "HTTP"} {
+
+		client := RunClient(ClientArgs{
+			grep:            "grep " + pattern,
+			outputDirectory: OUTPUT_PATH,
+			logsDirectory:   "data",
+			silence:         true,
+			command:         "",
+		})
+
+		for _, host := range client.hosts {
+			if host.status == STATUS_SUCCESS {
+				cmd := fmt.Sprintf("grep -c %s data/vm%s.log", pattern, host.id)
+				fmt.Println("Command: ", cmd)
+				out, err := exec.Command(cmd).Output()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				expected := int(strings.TrimSpace(out))
+
 				if host.lines != count {
 					t.Fail()
 				}
