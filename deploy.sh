3cba710f5acba2c7cce7a493cca7dfbad8911876
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-09-20 19:18:12 -0500

    testing deploy

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+passwd_file="/home/aarya/passwd"
+netid="aaryab2"
+
+if [ ! -f $passwd_file ]; then
+	echo Password file not found: $passwd_file
+	exit 1
+fi
+
+cluster=(
+	"fa23-cs425-0701.cs.illinois.edu"
+	"fa23-cs425-0702.cs.illinois.edu"
+	# "fa23-cs425-0703.cs.illinois.edu"
+	# "fa23-cs425-0704.cs.illinois.edu"
+	# "fa23-cs425-0705.cs.illinois.edu"
+	# "fa23-cs425-0706.cs.illinois.edu"
+	# "fa23-cs425-0707.cs.illinois.edu"
+	# "fa23-cs425-0708.cs.illinois.edu"
+	# "fa23-cs425-0709.cs.illinois.edu"
+	# "fa23-cs425-0710.cs.illinois.edu"
+)
+
+timeout=3
+SSH_OPTION="StrictHostKeyChecking no"
+
+for vm in "${cluster[@]}"; do
+	echo "vm: $vm"
+	ping -W $timeout -c 1 $vm
+
+	if [ $? -eq 0 ]; then
+		sshpass -f $passwd_file rsync -avu -e "ssh -o $SSH_OPTION" start.sh $netid@$vm:~/start.sh
+		sshpass -f $passwd_file ssh -f -n -o $SSH_OPTION $netid@$vm "./start.sh"
+	else
+		echo "Failed to connect to $vm"
+	fi
+done
+
---
+++ start.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+MP1_LOGS=$HOME/mp1.log
+MP2_LOGS=$HOME/mp2.log
+
+MP1_PORT=3000
+MP2_PORT=6000
+
+CS425_REPO=$HOME/cs425
+
+cd $HOME
+
+mkdir -p .ssh/
+
+[ -e ssh_cs425 ] && mv ssh_cs425 .ssh/cs425
+[ -e ssh_cs425.pub ] && mv ssh_cs425.pub .ssh/cs425.pub
+[ -e ssh_config ] && mv ssh_config .ssh/config
+
+if [ ! -d $CS425_REPO ]; then
+	git clone ssh://git@gitlab.engr.illinois.edu/aaryab2/cs425.git cs425
+	if [ ! $? -eq 0 ]; then
+		echo "Failed to clone repository"
+		exit 1
+	fi
+fi
+
+cd $CS425_REPO
+git checkout main
+git pull origin main
+
+cd $CS425_REPO/mp1
+make clean; make
+pkill -f bin/server
+nohup bin/server $MP1_PORT >$MP1_LOGS 2>&1 &
+echo "MP1 Server is running at $(hostname):$MP1_PORT$"
+
+cd $CS425_REPO/mp2
+make clean; make
+pkill -f ./main
+nohup ./main $(hostname) $MP2_PORT >$MP2_LOGS 2>&1 &
+echo "MP2 Server is running at $(hostname):$MP2_PORT$"
+
