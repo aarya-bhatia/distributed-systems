81a2efecc8bbeb0526e6e4384364540467b5d13b
Xinze-Zheng <xinzzheng.work@gmail.com>
2023-09-24 15:13:12 -0500

    update script

--- mp2/scripts/test.py
+++ mp2/scripts/test.py
@@ -4,7 +4,7 @@ import socket
 hosts = {}
 
 def print_usage():
-    print(f"<cmd> <target> <val>")
+    print(f"set <target>")
 
 def load_server():
     with open("hosts", "r") as f:
@@ -13,11 +13,12 @@ def load_server():
             words = line.split(" ")
             hosts[words[0]] = {"host_name": words[1], "port": words[2]}
     print(hosts)
-    print()
+
 def main():
     load_server()
     for line in stdin:
-        words = line.split(" ")
+        line = line.strip()
+        words = line.split(";")
         if len(words) < 2:
             print_usage()
             continue
@@ -26,18 +27,25 @@ def main():
         if target == 'all':
             target = range(1, 11)
         else:
-            target = [int(words[1])]
-
-        if cmd == "droprate":
-            for host in target:
-                host = str(host)
-                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                message = f"CONFIG DROPRATE {words[2]}"
-                print(f"sending {message}")
-                # sock.sendto(bytes(message, "utf-8"), (hosts[host]["host_name"], int(hosts[host]["port"])))
-                sock.sendto(bytes("id", "utf-8"), (hosts[host]["host_name"], int(hosts[host]["port"])))
+            vms = words[1].split(',')
+            target = []
+            for vm in vms:
+                target.append(vm)
+        print(f"sending {cmd} to {target}")
+        for host in target:
+            host = str(host)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.settimeout(0.2)
+            
+            host_name = hosts[host]["host_name"]
+            host_port = int(hosts[host]["port"])
+            sock.sendto(bytes(cmd, "utf-8"), (host_name, host_port))
+            print("-------")
+            try:
                 data, server = sock.recvfrom(1024)
-                print(str(data))
+                print(f'{host_name}:{host_port} {data.decode()}')
+            except socket.timeout:
+                print(f'{host_name}:{host_port} No response in 0.2 seconds')
 
 
 if __name__ == '__main__':
