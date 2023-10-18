ae2aed386d511bc5a114e65b17a73a5b8fd6cddc
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-10-18 09:17:13 -0500

    client specify command in args

--- go/client/main.go
+++ go/client/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"cs425/common"
 	"fmt"
 	"io"
 	"net"
 	"os"
-	"strings"
 	"time"
 )
 
@@ -132,62 +130,61 @@ func UploadFile(localFilename string, remoteFilename string) bool {
 
 func printUsage() {
 	fmt.Println("Usage:")
-	fmt.Println("Upload file: put local_filename remote_filename")
-	fmt.Println("Download file: get remote_filename local_filename")
-	fmt.Println("Query file: query remote_filename")
+	fmt.Println("Upload file: put <local_filename> <remote_filename>")
+	fmt.Println("Download file: get <remote_filename> <local_filename>")
+	fmt.Println("Query file: query <remote_filename>")
 }
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
 	if os.Getenv("prod") == "true" {
 		common.Cluster = common.ProdCluster
 	} else {
 		common.Cluster = common.LocalCluster
 	}
 
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		tokens := strings.Split(line, " ")
-		verb := tokens[0]
-		if verb == "help" {
+	if len(os.Args) == 1 {
+		printUsage()
+		return
+	}
+
+	tokens := os.Args[1:]
+	verb := tokens[0]
+
+	if verb == "put" {
+		if len(tokens) != 3 {
 			printUsage()
-		} else if verb == "put" {
-			if len(tokens) != 3 {
-				printUsage()
-				continue
-			}
-
-			startTime := time.Now().UnixNano()
-
-			if !UploadFile(tokens[1], tokens[2]) {
-				Log.Warn("Failed to upload file")
-			} else {
-				Log.Debug("Success!")
-				endTime := time.Now().UnixNano()
-				elapsed := endTime - startTime
-				fmt.Println("Upload time (sec): ", float64(elapsed)*1e-9)
-			}
-
-		} else if verb == "get" {
-			if len(tokens) != 3 {
-				printUsage()
-				continue
-			}
-
-			startTime := time.Now().UnixNano()
-
-			if !DownloadFile(tokens[2], tokens[1]) {
-				Log.Warn("Failed to download file ", tokens[2])
-			} else {
-				Log.Debug("Success!")
-				endTime := time.Now().UnixNano()
-				elapsed := endTime - startTime
-				fmt.Println("Download time (sec): ", float64(elapsed)*1e-9)
-			}
-		} else {
-			Log.Warn("Unknown command ", verb)
-			printUsage()
+			return
 		}
+
+		startTime := time.Now().UnixNano()
+
+		if !UploadFile(tokens[1], tokens[2]) {
+			Log.Warn("Failed to upload file")
+		} else {
+			Log.Debug("Success!")
+			endTime := time.Now().UnixNano()
+			elapsed := endTime - startTime
+			fmt.Println("Upload time (sec): ", float64(elapsed)*1e-9)
+		}
+
+	} else if verb == "get" {
+		if len(tokens) != 3 {
+			printUsage()
+			return
+		}
+
+		startTime := time.Now().UnixNano()
+
+		if !DownloadFile(tokens[2], tokens[1]) {
+			Log.Warn("Failed to download file ", tokens[2])
+		} else {
+			Log.Debug("Success!")
+			endTime := time.Now().UnixNano()
+			elapsed := endTime - startTime
+			fmt.Println("Download time (sec): ", float64(elapsed)*1e-9)
+		}
+	} else {
+		Log.Warn("Unknown command ", verb)
+		printUsage()
 	}
 }
