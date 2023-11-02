3bb66a2ecf64676293629383ad8c80d268bd4d3d
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-11-02 12:09:41 -0500

    fix deploy

--- NOTES.md
+++ NOTES.md
@@ -4,4 +4,6 @@
 - handle all stdin requests - as demo
 - handle all requests on network as well - testing
 - client - retry upload/download on failures - unless not enough nodes
+- client - caching on retry - if have time
+- server - persistent connections with all replicas forever
 
--- start.sh
+++ start.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 GIT_BRANCH=main
-CS425_REPO=$HOME/cs425
-MP1_LOGS=$HOME/mp1.log
-LOGS=$HOME/log
 
 cd $HOME
 rm -rf log *.log *.out data
 
-cd $CS425_REPO/go
+cd $HOME/cs425/go
+
 git reset HEAD
 git checkout $GIT_BRANCH || git checkout -b $GIT_BRANCH
 git pull origin $GIT_BRANCH
+go mod tidy
 
 if pgrep -f cs425 >/dev/null; then
-	echo KILL | nc localhost 5000 2>/dev/null
-	echo KILL | nc localhost 3000 2>/dev/null
+	echo KILL | nc localhost 5000
+	echo KILL | nc localhost 3000
 fi
 
-go mod tidy
-nohup go run cs425/shell/server $MP1_PORT >$MP1_LOGS 2>&1 &
-sleep 5
-nohup go run cs425 >$LOGS 2>&1 &
+pkill -f cs425
 
+sleep 5
+nohup go run . >$HOME/log 2>&1 &
+
+cd shell/server
+nohup go run . >$HOME/mp1.log 2>&1 &
+
+echo node $(hostname) is online
