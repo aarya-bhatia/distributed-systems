ee597de782e67e2976a5823624a97cec17f59af3
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-11-27 11:24:18 -0600

    update scripts

--- start.sh
+++ start.sh
@@ -5,21 +5,24 @@ cd $HOME
 rm -rf log *.log *.out *.dat data/
 
 cd $HOME/cs425/go
-git reset HEAD
+git reset --hard HEAD
 git checkout $GIT_BRANCH || git checkout -b $GIT_BRANCH
 git pull origin $GIT_BRANCH
 go mod tidy
 
-if pgrep -f cs425 >/dev/null; then
+if pgrep -f 'go run' >/dev/null; then
 	echo KILL >/dev/tcp/localhost/3000
 	echo KILL >/dev/udp/localhost/4000
 fi
 
 cd $HOME/cs425/go/main/filesystem/server
-nohup go run . >$HOME/log 2>&1 &
+nohup go run . >$HOME/mp3.log 2>&1 &
 
 cd $HOME/cs425/go/shell/server
 nohup go run . >$HOME/mp1.log 2>&1 &
+
+cd $HOME/cs425/go/main/maplejuice/server
+nohup go run . >$HOME/mp4.log 2>&1 &
 
 echo node $(hostname) is online
 
