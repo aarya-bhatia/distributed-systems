b1a5c6646056053d96f43ebbce273cbcde66ebb1
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-11-27 09:45:15 -0600

    update script

--- go/main/filesystem/client/tasks.sh
+++ go/main/filesystem/client/tasks.sh
@@ -3,7 +3,8 @@ go run . 1 rmdir /
 go run . 1 put data/sample sample
 go run . 1 put data/vm1 vm1
 
-for file in $(ls *.py); do
-	go run . 1 put $file $file
+exe_dir=./maplejuice_exe
+for file in $(ls ${exe_dir}/*.py); do
+	go run . 1 put $file $(basename $file)
 done
 
